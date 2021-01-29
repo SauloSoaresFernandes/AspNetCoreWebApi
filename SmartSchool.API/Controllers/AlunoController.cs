5bfac6bd74dd679aaa4946bfc54760770cf2cb8d
LOCALIZA_CORP\196445 <squadra184@localiza.com>
2021-01-29 15:08:01 -0300

    Adicionando projeto no GitHUB

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SmartSchool.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno()
+            {
+                Id = 1,
+                Nome = "Marcos",
+                Sobrenome = "Silva",
+                Telefone = "354895214"
+            },
+             new Aluno()
+            {
+                Id = 2,
+                Nome = "Marta",
+                Sobrenome = "Mendes",
+                Telefone = "665147852"
+            },
+              new Aluno()
+            {
+                Id = 3,
+                Nome = "Laura",
+                Sobrenome = "Gusmão",
+                Telefone = "9912548756"
+            },
+        };
+        public AlunoController() { }
+
+        // GET: api/<AlunoController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        // api/aluno/byId
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("O Aluno não foi encontrado.");
+
+            return Ok(aluno);
+        }
+
+        // api/aluno/nome
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string Nome, string Sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(Nome) && a.Sobrenome.Contains(Sobrenome));
+            if (aluno == null) return BadRequest("O Aluno não foi encontrado.");
+
+            return Ok(aluno);
+        }
+
+        // api/aluno
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        // api/aluno
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        // api/aluno
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        // api/aluno
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
